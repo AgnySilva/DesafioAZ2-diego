6a6f942cab4a1ec9cddd4fb1e13b766dfa9e4bdd
AgnySilva <33436723+AgnySilva@users.noreply.github.com>
2018-07-30 16:26:46 -0300

    Add files via upload

--- relatorio.php
+++ relatorio.php
@@ -1,19 +1,21 @@
 ﻿<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<table class="table">
-  <tr>
-    <th>Qtde Clientes</th>
-    <th>Qtde Vendedores</th> 
-    <th>Media Salarial (R$)</th>
-	<th>Maior Venda (ID)</th>
-	<th>Pior Vendedor</th>
-  </tr>
-  <tr>
-	<?php
-	echo"<td>$contadorC</td>";
-	echo"<td>$contadorSM</td>";
-	echo"<td>$mediaSalario</td>";
-	echo"<td>$idMaior</td>";
-	echo"<td>$idPior</td>";
-	?>
+<div class="form-group col-sm-10 col-lg-offset-1">
+	<table class="table">
+	  <tr>
+		<th>Qtde Clientes</th>
+		<th>Qtde Vendedores</th> 
+		<th>Media Salarial (R$)</th>
+		<th>Maior Venda (ID)</th>
+		<th>Pior Vendedor</th>
+	  </tr>
+	  <tr>
+		<?php
+		echo"<td>$contadorC</td>";
+		echo"<td>$contadorSM</td>";
+		echo"<td>$mediaSalario</td>";
+		echo"<td>$idMaior</td>";
+		echo"<td>$idPior</td>";
+		?>
 
-</table>
+	</table>
+</div>
